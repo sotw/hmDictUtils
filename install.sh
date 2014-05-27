43d3fe61079822f80c09b09ddeb17592787d0487
Hammer <workthethird@gmail.com>
2014-05-27 17:21:17 +0800

    add google search prototype

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@ INSFOLDER=~/.hmDict
 rm -Rf $INSFOLDER
 rm -f ~/bin/sh/edict
 rm -f ~/bin/sh/jdict
+rm -f ~/bin/sh/google
 mkdir -p ~/bin/sh
 mkdir -p $INSFOLDER
 cp -Rf jianfan $INSFOLDER
@@ -9,6 +10,7 @@ cp -Rf jNlp $INSFOLDER
 cp *.py $INSFOLDER
 cp edict ~/bin/sh
 cp jdict ~/bin/sh
+cp google ~/bin/sh
 cp argumentDbA $INSFOLDER/argumentDbA
 cp argumentDbB $INSFOLDER/argumentDbB
 chmod 755 -R $INSFOLDER
