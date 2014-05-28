f77d53f2ec7c55d6a4ae6cd75f0bdcf6043aac4c
Onishima Tatsuya <workthethird@gmail.com>
2014-05-29 03:35:44 +0800

    add uninstall.sh

---
+++ uninstall.sh
@@ -0,0 +1,9 @@
+INSFOLDER=~/.hmDict
+echo "You can't be serious, but what is done is done."
+rm -Rf $INSFOLER
+rm -f ~/bin/sh/edict
+rm -f ~/bin/sh/jdict
+rm -f ~/bin/sh/google
+rm -f ~/bin/sh/wiki
+rm -f ~/bin/sh/getPOEReleaseNote
+echo "Bye, have a nice day"
