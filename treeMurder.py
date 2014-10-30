ff054fa5272b3783145ab6749266be2c9078006d
Nostrum <workthethird@gmail.com>
2014-10-30 12:15:41 +0800

    add treeMurer

--- treeMurder.py
+++ treeMurder.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 import urllib, urllib2
 import curses
+import shutil
 #import textwrap
 from os.path import expanduser
 from subprocess import PIPE
@@ -33,86 +34,86 @@ ARGUDB = []
 _wrap = TextWrapper()
 _wrap.width = 34
 
-class Menu(object):                                                          
+class Menu(object):
 
-    def __init__(self, items, stdscreen):                                    
-        self.window = stdscreen.subwin(0,0)                                  
-        self.window.keypad(1)                                                
-        self.panel = panel.new_panel(self.window)                            
-        self.panel.hide()                                                    
-        panel.update_panels()                                                
+    def __init__(self, items, stdscreen):
+        self.window = stdscreen.subwin(0,0)
+        self.window.keypad(1)
+        self.panel = panel.new_panel(self.window)
+        self.panel.hide()
+        panel.update_panels()
 
-        self.position = 0                                                    
-        self.items = items                                                                      
+        self.position = 0
+        self.items = items
         self.items.append(('==== OUTPUT ==== (press "q" for exit)','nothing'))
         self.items.append(('>','nothing'))
 
-    def navigate(self, n):                                                   
-        self.position += n                                                   
-        if self.position < 0:                                                
+    def navigate(self, n):
+        self.position += n
+        if self.position < 0:
             self.position = len(self.items)-3
-        elif self.position > len(self.items)-3:                               
+        elif self.position > len(self.items)-3:
             self.position = 0
 
-    def display(self):                                                       
-        self.panel.top()                                                     
-        self.panel.show()                                                    
-        self.window.clear()                                                  
+    def display(self):
+        self.panel.top()
+        self.panel.show()
+        self.window.clear()
 
-        while True:                                                          
-            self.window.refresh()                                            
-            curses.doupdate()                                                            
-            for index, item in enumerate(self.items):                        
-                if index == self.position:                                   
-                    mode = curses.A_REVERSE                                  
-                else:                                                        
-                    mode = curses.A_NORMAL                                   
+        while True:
+            self.window.refresh()
+            curses.doupdate()
+            for index, item in enumerate(self.items):
+                if index == self.position:
+                    mode = curses.A_REVERSE
+                else:
+                    mode = curses.A_NORMAL
 
                 if index < len(self.items)-2:
-                	msg = '%s. %s' % (str(index).zfill(2), item[0])                            
-                	self.window.addstr(1+index, 1, msg, mode)                    
+                	msg = '%s. %s' % (str(index).zfill(2), item[0])
+                	self.window.addstr(1+index, 1, msg, mode)
                 else:
-                	msg = '%s' % (item[0])                            
-                	self.window.addstr(1+index, 1, msg, mode)                    
+                	msg = '%s' % (item[0])
+                	self.window.addstr(1+index, 1, msg, mode)
 
-            key = self.window.getch()                                        
+            key = self.window.getch()
 
-            if key in [curses.KEY_ENTER, ord('\n')]:                         
-                if self.position == len(self.items)-3:                       
-                    break                                                    
+            if key in [curses.KEY_ENTER, ord('\n')]:
+                if self.position == len(self.items)-3:
+                    break
                 elif self.position == len(self.items)-2:
                 	continue
                 elif self.position == len(self.items)-1:
                 	continue
-                else:                                                        
+                else:
                 	self.items.pop()
                 	self.items.append((self.items[self.position][1](self.items[self.position][0]),'result'))
 
-            elif key == curses.KEY_UP:                                       
-                self.navigate(-1)                                            
+            elif key == curses.KEY_UP:
+                self.navigate(-1)
 
-            elif key == curses.KEY_DOWN:                                     
-                self.navigate(1)                                             
+            elif key == curses.KEY_DOWN:
+                self.navigate(1)
 
             elif key == ord('q'):
             	break
 
-        self.window.clear()                                                  
-        self.panel.hide()                                                    
-        panel.update_panels()                                                
+        self.window.clear()
+        self.panel.hide()
+        panel.update_panels()
         curses.doupdate()
 
 
-class MyApp(object):	
-	def __init__(self, stdscreen):		
+class MyApp(object):
+	def __init__(self, stdscreen):
 		self.screen = stdscreen
 		try:
 			curses.curs_set(0)
 		except:
-			print "don\'t support curs_set"			
+			print "don\'t support curs_set"
 
 		filenames = []
-		for (dirpath, dirnames, filenames) in os.walk(tTarget):		
+		for (dirpath, dirnames, filenames) in os.walk(tTarget):
 			break
 
 		#print filenames
@@ -135,29 +136,40 @@ class MyApp(object):
 			self.__main_menu = Menu([], self.screen)
 			self.__main_menu.display()
 
-def doSomeTrick(filename):				
+def doSomeTrick(filename):
 	result = 'doSomeTrick'
 	f = open(filename,'r')
 	if f is not None:
 		content = f.read()
-		result = applyTrick(content)		
+		result = applyTrick(content)
 		f.close()
 	else:
-		result = 'can\'t open '+filename+'!'	
+		result = 'can\'t open '+filename+'!'
 	return result
 
 def applyTrick(content):
 	result = 'trick applied'
-	#do some trick here		
+	#do some trick here
 	return result
 
-def doStuff():	
+def doStuff():
 	curses.wrapper(MyApp)
 
 def doStuffDirect():
 	DB.debug('Dirct execute')
-	ret = doSomeTrick(tTarget)
-	DB.debug(ret)
+	answer=raw_input('Do you know what you are doing!!(y)')
+	if answer == 'y':
+		filenames = []
+		for (dirpath, dirnames, filenames) in os.walk(tTarget):
+			if len(dirnames) != 0:
+				print dirnames
+				for dirname in dirnames:
+					print 'dealing with dirname:'+dirname
+					if '.git' in dirname or '.repo' in dirname:
+						print "gonna rm -rf this dir:"+dirpath+"/"+dirname
+						shutil.rmtree(dirpath+'/'+dirname)
+	else :
+		print 'GOOD'
 
 def setup_logging(level):
 	global DB
@@ -176,12 +188,12 @@ def verify():
 	parser.add_argument('query', nargs='*', default=None)
 	parser.add_argument('-d', '--database', dest='database', action = 'store', default='/.hmDict/treeMurder.db') #replace
 	args = parser.parse_args()
-	tTarget = ' '.join(args.query)	
+	tTarget = ' '.join(args.query)
 	log_level = logging.INFO
 	if args.verbose:
 		log_level = logging.DEBUG
 	if not tTarget:
-		tTarget = os.getcwd()		
+		tTarget = os.getcwd()
 		#print tTarget
 		#raw_input()
 	else :
