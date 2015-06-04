6f9f6489284d765549bdcb5a88664f87251a214b
Nostrum <workthethird@gmail.com>
2015-06-04 17:37:41 +0800

    a emergency change for new yahoo, adopt beautifulsoup4

--- edict.py
+++ edict.py
@@ -11,6 +11,7 @@ from os.path import expanduser
 from cStringIO import StringIO
 from lxml import etree
 from pprint import pprint
+from bs4 import BeautifulSoup,NavigableString
 
 global DB
 global args
@@ -38,79 +39,152 @@ def cp65001(name):
 codecs.register(cp65001)
 #codecs.register(cp950)
 
+def strip_tags(html, invalid_tags):
+    stripSoup = BeautifulSoup(html)
+
+    for tag in stripSoup.findAll(True):
+        if tag.name in invalid_tags:
+            s = ""
+
+            for c in tag.contents:
+                if not isinstance(c, NavigableString):
+                    c = strip_tags(unicode(c), invalid_tags)
+                s += unicode(c)
+
+            tag.replaceWith(s)
+    return stripSoup
+
 def repeatStr(string_to_expand, length):
 	return (string_to_expand * ((length/len(string_to_expand))+1))[:length]
 
 def htmlParser(tPage):
-	resp = urllib2.urlopen(tPage)
-	if resp.code == 200 :
-  	  data = resp.read()
-  	  resp.close()
-	elif resp.code == 404 :
-  	  print "page do not exist"
-  	  exit()
-	else :
-  	  print "can not open page"
-  	  exit()
-	parser = etree.HTMLParser()
-	tree = etree.parse(StringIO(data), parser)
-	#etree.strip_tags(tree,'strong')
-	etree.strip_tags(tree,'samp')
-	result = etree.tostring(tree.getroot(), pretty_print=True, method="html", encoding="utf-8")
-	
-	#print repr(result)
-	global mProun
-	mProun = re.findall('<span class="proun_type">(.+?)</span><span class="proun_value">(.+?)</span>',result)
+    resp = urllib2.urlopen(tPage)
+    if resp.code == 200 :
+        data = resp.read()
+        resp.close()
+    elif resp.code == 404 :
+        print "page do not exist"
+        exit()
+    else :
+        print "can not open page"
+        exit()
 
-	#print len(mProun)
-	#raw_input()
-	mTitle = re.findall('<span class="yschttl">(.+?)</span>',result)
+    soup = BeautifulSoup(''.join(data))
 
-	etree.strip_tags(tree,'span')
-	etree.strip_tags(tree,'a')
+    print "beautifulSoup result"
+    resultAry = soup.findAll('div',{'class':'dd algo mt-20 lst DictionaryResults'});
+    print resultAry[0].get_text()
+    #result = resultAry[0].renderContents()
+    #print result
+    #print type(result)
+    #invalid_tags = ['b','div']
 
-	
-	for p in tree.xpath("//p"):
-  	  p.tail = 'breakHere' + p.tail if p.tail else 'breakHere'
-	etree.strip_tags(tree,'p')
-	etree.strip_tags(tree,'b')
-	for h5 in tree.xpath("//h5"):
-   	   h5.tail = 'titleBreak' + h5.tail if h5.tail else 'titleBreak'
-	etree.strip_tags(tree,'h5')
-	etree.strip_tags(tree,'ol')
-	etree.strip_tags(tree,'li')
-	etree.strip_tags(tree,'code')
+    #print strip_tags(result, invalid_tags)
+    #soup2 = BeautifulSoup(''.join(layer1));
+    #print soup2.
 
-	#result = etree.tostring(tree.getroot(), pretty_print=True, method="html")
-	#DB(1, result)
 
-	resultSet = []
-	targetURL = ""
-	lineSum = 0
-	myList = tree.xpath("//ul[@class='explanation_wrapper']")
-	for e in myList:
-		if e.text is not None:
-			resultSet.append(e.text)
+def htmlParserBak(tPage):
+    resp = urllib2.urlopen(tPage)
+    if resp.code == 200 :
+        data = resp.read()
+        resp.close()
+    elif resp.code == 404 :
+        print "page do not exist"
+        exit()
+    else :
+        print "can not open page"
+        exit()
 
-	for tTitle in mTitle:
-		resultSet.insert(0,clrTx(tTitle,'AUQA')+'\n')
+    soup = BeautifulSoup(''.join(data))
+    #body = soup.contents[0].contents[1]
 
-	guessList = tree.xpath("//h2/i")
-	if len(guessList) != 0 :
-   	   for e in guessList :
-   	   	   if e.text is not None:
-   	   	   	   resultSet.append('\n You mean:'+e.text+' ?')
-   	   	   	   #print '\n You mean:'+e.text+' ?'
+    print "beautifulSoup result"
+    print soup.findAll('div', {'class':'dd algo mt-20 lst DictionaryResults'});
 
-	return resultSet
+    parser = etree.HTMLParser()
+    tree = etree.parse(StringIO(data), parser)
+    etree.strip_tags(tree,'strong')
+    #etree.strip_tags(tree,'samp')
+    result = etree.tostring(tree.getroot(), pretty_print=True, method="html", encoding="utf-8")
 
-#[]== maybe textwrapper, it's better than this hardcode 
-def prettyPrint(resultSet):	
+    #print repr(result)
+    #raw_input()
+    #global mProun
+    #mProun = re.findall('<span class="proun_type">(.+?)</span><span class="proun_value">(.+?)</span>',result)
+
+    #print len(mProun)
+    #raw_input()
+    #mTitle = re.findall('<span class="yschttl">(.+?)</span>',result)
+
+    etree.strip_tags(tree,'span')
+    etree.strip_tags(tree,'a')
+
+
+    #for p in tree.xpath("//p"):
+        #p.tail = 'breakHere' + p.tail if p.tail else 'breakHere'
+    etree.strip_tags(tree,'p')
+    etree.strip_tags(tree,'b')
+    #for h5 in tree.xpath("//h5"):
+        #h5.tail = 'titleBreak' + h5.tail if h5.tail else 'titleBreak'
+    #etree.strip_tags(tree,'h5')
+    #etree.strip_tags(tree,'ol')
+    etree.strip_tags(tree,'li')
+    etree.strip_tags(tree,'ul')
+    #etree.strip_tags(tree,'code')
+    #etree.strip_tags(tree,'div')
+    etree.strip_tags(tree,'h3')
+    etree.strip_tags(tree,'h4')
+
+    result = etree.tostring(tree.getroot(), pretty_print=True, method="html", encoding="utf-8")
+    print result
+    #raw_input()
+
+    #replacedStr = re.sub('<div class="compTitle mb-10">','\n',result);
+
+    #newResults = re.findall('<div class="dd algo.+?DictionaryResults".+?>(.+?)</div>',replacedStr,re.DOTALL)
+
+
+
+    #print '========'
+    #for newResult in newResults:
+    #    print newResult[0]
+    #    print '====='
+    #    print newResult[1]
+
+    resultSet = []
+    targetURL = ""
+    lineSum = 0
+
+
+    myList = tree.xpath("//div[@class='dd algo mt-20 lst DictionaryResults']/")
+    for e in myList:
+        print e.tag
+        if e.text is not None:
+            print e.text
+    #for e in myList:
+    #    if e.text is not None:
+    #        resultSet.append(e.text)
+
+    #for tTitle in mTitle:
+    #    resultSet.insert(0,clrTx(tTitle,'AUQA')+'\n')
+
+    #guessList = tree.xpath("//h2/i")
+    #if len(guessList) != 0 :
+    #    for e in guessList :
+    #        if e.text is not None:
+    #            resultSet.append('\n You mean:'+e.text+' ?')
+    #            #print '\n You mean:'+e.text+' ?'
+
+    return resultSet
+
+#[]== maybe textwrapper, it's better than this hardcode
+def prettyPrint(resultSet):
 	if bWindows :
 		os.system('cls')
 	else:
 		os.system('clear')
-	
+
 	passIstring = ''
 
 	print " "
@@ -119,7 +193,7 @@ def prettyPrint(resultSet):
 		result = result.replace('titleBreak','\n')
 		#result = result.replace('idiom','\n\ridiom')
 		#print len(result)
-		for tag in ARGUDB:					    	
+		for tag in ARGUDB:
 			result = result.replace(tag,clrTx('\n'+tag,'YELLOW'))
 		if len(result) > 1 :
 			passIstring = passIstring+result
@@ -142,13 +216,13 @@ def prettyPrint(resultSet):
 
 		if pauseCnt <= 20 :
 			pauseCnt+=1
-		else :			
+		else :
 			raw_input()
 			pauseCnt=0
 
 		if totalCnt == 0 :
 			print repeatStr("-",len(strII))
-		try:		    
+		try:
 			if bWindows :
 				print strII+'\n'
 			else:
@@ -161,8 +235,8 @@ def prettyPrint(resultSet):
 			print repeatStr("-",len(strII))
 
 		if totalCnt == 0:
-			for proun in mProun:			    
-				print '    '+proun[0]+' '+proun[1]				
+			for proun in mProun:
+				print '    '+proun[0]+' '+proun[1]
 		totalCnt+=1
 
 def loadArgumentDb():
@@ -173,10 +247,10 @@ def loadArgumentDb():
 			for line in f:
 				if line != '\n' and line[0] != '#':
 					line = line.rstrip('\n')
-					global ARGUDB					
+					global ARGUDB
 					ARGUDB.append(line)
 			f.close()
-		else:			
+		else:
 			DB.error('db file open fail')
 	else :
 		print 'database doesn\'t existed'
@@ -185,7 +259,7 @@ def loadArgumentDb():
 
 def main():
    resultSet = htmlParser(tPage)
-   prettyPrint(resultSet)
+   #prettyPrint(resultSet)
 
 def setup_logging(level):
 	global DB
